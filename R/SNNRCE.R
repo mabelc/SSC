761a6b6cedd4d92d7de211a13b67e9640f310dd4
Osmani Rosado-Falcon <osmanir@uclv.cu>
2017-08-07 15:12:03 -0400

    Primera revision del codigo del SNNRCE. Se arreglo el calculo de la variable proportion. Se escribio parte de la ayuda de la funcion de entrenamiento. Se renombro la variable y.new por ynew. Se elimino la creacion de la variable cantClass al inicio porque no se usa.

--- R/SNNRCE.R
+++ R/SNNRCE.R
@@ -1,20 +1,54 @@
 
-
+#' @title Train a SNNRCE model
+#' @description Trains a model for classification,
+#' according to SNNRCE algorithm.
+#' @param x A matrix or a dataframe with the training instances.
+#' @param y A vector with the labels of training instances. In this vector the unlabeled instances
+#' are specified with the value \code{NA}.
+#' @param dist The name of a distance method available in \code{proxy} package or
+#' a function defined by the user that computes the distance between two instances.
+#' Default is Euclidean distance.
+#' @param alpha Rejection threshold to test the critical region. Default is 0.1.
+#' @return The trained model.
 #' @export
 snnrce <- function(
   x, y,
-  dist,
+  dist = "Euclidean",
   alpha = 0.1
 ){
+  ### Check parameters ###
+  # Check x
+  if(!is.matrix(x) && !is.data.frame(x)){
+    stop("Parameter x is neither a matrix or a data frame.")
+  }
   # Check y 
   if(!is.factor(y)){
     stop("Parameter y is not a factor. Use as.factor(y) to convert y to a factor.")
   }
-  # Determine the indexes of labeled instances
+  # Check relation between x and y
+  if(nrow(x) != length(y)){
+    stop("The rows number of x must be equal to the length of y.")
+  }
+  # Check dist
+  # This check is made by proxy::dist function in Init variables section
+  # Check alpha
+  if(!(alpha >= 0 && alpha <= 1)) {
+    stop("Parameter alpha must be between 0 and 1")
+  }
+  
+  ### Init variables ###
+  # Compute distance matrix D
+  D <- as.matrix(
+    proxy::dist(x = x, method = dist, diag = TRUE, upper = TRUE, by_rows = TRUE)
+  )
+  
+  # Init variable to store the labels
+  ynew <- y
+  
+  # Obtain the indexes of labeled and unlabeled instances
   labeled <- which(!is.na(y))
-  # Determine the indexes of unlabeled examples
   unlabeled <- which(is.na(y))
-  ## Check the labeled and unlabeled sets
+  # Check the labeled and unlabeled sets
   if(length(labeled) == 0){   # labeled is empty
     stop("The labeled set is empty. All the values in y parameter are NA.")
   }
@@ -22,44 +56,22 @@ snnrce <- function(
     stop("The unlabeled set is empty. None value in y parameter is NA.")
   }
   
-  # Check x
-  if(!is.matrix(x) && !is.data.frame(x)){
-    stop("Parameter x is neither a matrix or a data frame.")
-  }
-  # Check relation between x and y
-  if(nrow(x) != length(y)){
-    stop("The rows number of x must be equal to the length of y.")
-  }
-  
-  # Check alpha
-  if(!(alpha >= 0 && alpha <= 1)) {
-    stop("alpha must be between 0 and 1")
-  }
-  
-  # Compute distance matrix D
-  D <- as.matrix(
-    proxy::dist(x = x, method = dist, diag = TRUE, upper = TRUE, by_rows = TRUE)
-  )
-  
   # Identify the classes
   classes <- levels(y)
   nclasses <- length(classes)
   
-  # Count the examples per class
-  cls.summary <- summary(y[labeled])
-  # Determine the total of instances to include per iteration
-  cantClass <- round(cls.summary / min(cls.summary))
-  
-  # Init variable to increment the labeled set
-  y.new <- y
+  ### SNNRCE algorithm ###
   
   # Initial number of labeled instances
   labeledLen <- length(labeled)
+  
+  # STEPS 1-2
+  # Count the examples per class
+  cls.summary <- summary(y[labeled])
   # Ratio between count per class and the initial number of labeled instances
-  proportion <- cantClass / labeledLen
+  proportion <- cls.summary / labeledLen
   
-  ### STEP 3 ###
-  
+  # STEP 3
   # Label the instances with Rj = 0
   rem <- NULL
   for (i in 1:length(unlabeled)) {
@@ -88,26 +100,24 @@ snnrce <- function(
     
     if (good) {
       # etiquetar y eliminar de unlabeled
-      y.new[w] <- clase
+      ynew[w] <- clase
       rem <- c(rem,i)
     }
   }
-  
+
+  ## Update labeled and unlabeled sets
   labeled <- c(labeled, unlabeled[rem])
   unlabeled <- unlabeled[-rem]
   
-  nmax <- proportion * length(unlabeled)
-  inicialLen <- length(labeled)
+  # STEP 5 Autolabel
+  initialLen <- length(labeled)
   
-  # STEP 5 autolabel
-  
-  iter <- 0
-  cantClass <- rep(0, nclasses)
-  while (all(cantClass < nmax)) {
-    iter <- iter + 1
-    
+  max.per.class <- proportion * length(unlabeled)
+  nmax <- min(max.per.class)
+  count <- 0
+  while (count < nmax) {
     # Training 1-NN
-    model <- oneNN(y = y.new[labeled])
+    model <- oneNN(y = ynew[labeled])
     prob <- predict(model, D[unlabeled, labeled], type = "prob")
     
     # Select the instances with better class probability 
@@ -116,17 +126,18 @@ snnrce <- function(
     # Add selected instances to L
     labeled.prime <- unlabeled[selection$unlabeled.idx]
     sel.classes <- classes[selection$class.idx]
-    y.new[labeled.prime] <- sel.classes
+    ynew[labeled.prime] <- sel.classes
     labeled <- c(labeled, labeled.prime)
     
     # Delete selected instances from U
     unlabeled <- unlabeled[-selection$unlabeled.idx]
     
-    cantClass <- cantClass + 1
+    # Increment the number of labeled instances per class
+    count <- count + 1
   }
   
   len <- length(labeled)
-  if (inicialLen < len) { # new instances were added
+  if (initialLen < len) { # new instances were added
     # STEP 6 Build RNG for L
     
     ady <- vector("list", len) # Adjacency list of G
@@ -154,7 +165,7 @@ snnrce <- function(
       for (j in ady[[i]]) {
         Wij <- 1 / (1 + D[labeled[i], labeled[j]])
         I[i] <- I[i] + Wij
-        if (y.new[labeled[i]] != y.new[labeled[j]])
+        if (ynew[labeled[i]] != ynew[labeled[j]])
           J[i] <- J[i] + Wij
       }
     
@@ -172,24 +183,24 @@ snnrce <- function(
         wc <- rep(0, nclasses)
         for (j in ady[[i]]) {
           Wij <- 1 / (1 + D[labeled[i], labeled[j]])
-          pos <- unclass(y.new[labeled[j]])
+          pos <- unclass(ynew[labeled[j]])
           wc[pos] <- wc[pos] + Wij
         }
-        wc[unclass(y.new[labeled[i]])] <- 0
+        wc[unclass(ynew[labeled[i]])] <- 0
         w <- which.max(wc)
       } else { # if two classes invert the label
-        w <- ifelse(unclass(y.new[labeled[i]]) == 1, 2, 1)
+        w <- ifelse(unclass(ynew[labeled[i]]) == 1, 2, 1)
       }
       
       if (w != -1)
-        y.new[labeled[i]] <- classes[w]
+        ynew[labeled[i]] <- classes[w]
     }
     rm(ady)
   }
   
   result <- list(
-    tr.insts = x[labeled,],
-    tr.labels = y.new[labeled], 
+    xtrain = x[labeled,],
+    ytrain = ynew[labeled],
     dist = dist
   )
   class(result) <- "snnrce"
@@ -199,11 +210,11 @@ snnrce <- function(
 
 #' @export
 #' @importFrom stats predict
-predict.snnrce <- function(object, x, ...) {
-  D <- proxy::dist(x = x, y = object$tr.insts, method = object$dist, 
+predict.snnrce <- function(object, xtest, ...) {
+  D <- proxy::dist(x = xtest, y = object$xtrain, method = object$dist, 
                    diag = TRUE, upper = TRUE, by_rows = TRUE)
   
-  model <- oneNN(y = object$tr.labels)
+  model <- oneNN(y = object$ytrain)
   cls <- predict(model, D, type = "class")
   
   return(cls)
