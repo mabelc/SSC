08cab3c8b080c24ca5c7377e83aaa634ccb8d8f0
Osmani Rosado-Falcon <osmanir@uclv.cu>
2017-12-16 10:52:44 -0500

    Se creo la funcion snnrceBase (no se exporta). La funcion snnrce ahora es un wrapper de snnrceBase. snnrce se puede llamar ahora usando una matriz de instancias o una matriz de distancias.

--- R/SNNRCE.R
+++ R/SNNRCE.R
@@ -1,46 +1,34 @@
 
-#' @title Train a SNNRCE model
-#' @description Trains a model for classification,
-#' according to SNNRCE algorithm.
-#' @param x A matrix or a dataframe with the training instances.
-#' @param y A vector with the labels of training instances. 
-#' In this vector the unlabeled instances are specified with the value \code{NA}.
-#' @param dist The name of a distance method available in \code{proxy} package or
-#' a function defined by the user that computes the distance between two instances.
-#' Default is Euclidean distance.
-#' @param alpha Rejection threshold to test the critical region. Default is 0.1.
-#' @return The trained model.
-#' @export
-snnrce <- function(
-  x, y,
-  dist = "Euclidean",
+snnrceBase <- function(
+  D, y,
   alpha = 0.1
 ){
   ### Check parameters ###
-  # Check x
-  if(!is.matrix(x) && !is.data.frame(x)){
-    stop("Parameter x is neither a matrix or a data frame.")
-  }
   # Check y 
-  if(!is.factor(y)){
-    stop("Parameter y is not a factor. Use as.factor(y) to convert y to a factor.")
+  if(!is.factor(y) ){
+    if(!is.vector(y)){
+      stop("Parameter y is neither a vector nor a factor.")  
+    }else{
+      y = as.factor(y)
+    }
   }
-  # Check relation between x and y
-  if(nrow(x) != length(y)){
-    stop("The rows number of x must be equal to the length of y.")
+  # Check distance matrix
+  if(class(D) == "dist"){
+    D <- proxy::as.matrix(D)
   }
-  # Check dist
-  # This check is made by proxy::dist function in Init variables section
+  if(!is.matrix(D)){
+    stop("Parameter D is neither a matrix or a dist object.")
+  }else if(nrow(D) != ncol(D)){
+    stop("The matrix D is not square.")
+  }else if(nrow(D) != length(y)){
+    stop(sprintf(paste("The dimensions of the matrix D is %i x %i", 
+                       "and it's expected %i x %i according to the size of y."), 
+                 nrow(D), ncol(D), length(y), length(y)))
+  }
   # Check alpha
   if(!(alpha >= 0 && alpha <= 1)) {
     stop("Parameter alpha must be between 0 and 1")
   }
-  
-  ### Init variables ###
-  # Compute distance matrix D
-  D <- as.matrix(
-    proxy::dist(x = x, method = dist, diag = TRUE, upper = TRUE, by_rows = TRUE)
-  )
   
   # Init variable to store the labels
   ynew <- y
@@ -105,7 +93,7 @@ snnrce <- function(
       rem <- c(rem,  i)
     }
   }
-
+  
   ## Update labeled and unlabeled sets
   labeled <- c(labeled, unlabeled[rem])
   unlabeled <- unlabeled[-rem]
@@ -202,24 +190,89 @@ snnrce <- function(
   
   # Save result
   result <- list(
-    xtrain = x[labeled,],
-    ytrain = ynew[labeled],
-    dist = dist
+    model = oneNN(y = ynew[labeled]),
+    included.insts = labeled
   )
-  class(result) <- "snnrce"
+  
+  class(result) <- "snnrceBase"
   
   return(result)
 }
 
 #' @export
 #' @importFrom stats predict
-predict.snnrce <- function(object, xtest, ...) {
-  D <- proxy::dist(x = xtest, y = object$xtrain, method = object$dist, 
+predict.snnrceBase <- function(object, D, ...) {
+  if(class(D) == "dist"){
+    D <- proxy::as.matrix(D)
+  }
+  
+  cls <- predict(object$model, D, type = "class")
+  return(cls)
+}
+
+#' @export
+snnrce <- function(
+  x, y,
+  dist = "Euclidean",
+  x.dist = FALSE,
+  alpha = 0.1
+){
+  ### Check parameters ###
+  # Check x.dist
+  if(!is.logical(x.dist)){
+    stop("Parameter x.dist is not logical.")
+  }
+  
+  if(x.dist){
+    # Distance matrix case
+    # Check matrix distance in x
+    if(class(x) == "dist"){
+      x <- proxy::as.matrix(x)
+    }
+    if(!is.matrix(x)){
+      stop("Parameter x is neither a matrix or a dist object.")
+    } else if(nrow(x) != ncol(x)){
+      stop("The distance matrix x is not a square matrix.")
+    } else if(nrow(x) != length(y)){
+      stop(sprintf(paste("The dimensions of the matrix x is %i x %i", 
+                         "and it's expected %i x %i according to the size of y."), 
+                   nrow(x), ncol(x), length(y), length(y)))
+    }
+    
+    result <- snnrceBase(D = x, y, alpha)
+  }else{
+    # Instance matrix case
+    # Check x
+    if(!is.matrix(x) && !is.data.frame(x)){
+      stop("Parameter x is neither a matrix or a data frame.")
+    }
+    # Check relation between x and y
+    if(nrow(x) != length(y)){
+      stop("The rows number of x must be equal to the length of y.")
+    }
+    
+    result <- snnrceBase(
+      D = proxy::dist(x, method = dist, by_rows = TRUE,
+                      diag = TRUE, upper = TRUE), 
+      y, 
+      alpha
+    )
+    
+    class(result) <- "snnrce"
+    result$xtrain <- x[result$included.insts, ]
+    result$dist <- dist
+  }
+  
+  return(result)
+}
+
+#' @export
+#' @importFrom stats predict
+predict.snnrce <- function(object, x, ...) {
+  D <- proxy::dist(x, y = object$xtrain, method = object$dist, 
                    diag = TRUE, upper = TRUE, by_rows = TRUE)
   
-  model <- oneNN(y = object$ytrain)
-  cls <- predict(model, D, type = "class")
-  
+  cls <- predict(object$model, D, type = "class")
   return(cls)
 }
 
