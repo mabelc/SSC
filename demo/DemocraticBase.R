123ed9948d104cee08dab8b74b55167bd469032a
Osmani Rosado-Falcon <osmanir@uclv.cu>
2018-01-04 11:30:33 -0500

    Se agregaron los ejemplos de democraticBase.

---
+++ demo/DemocraticBase.R
@@ -0,0 +1,146 @@
+#TODO: revisar los ejemplos a y b no dan igual resultado
+# La diferencia esta en las maquinas de soporte vectorial.
+library(ssc)
+
+## Load Wine data set
+data(wine)
+
+cls <- which(colnames(wine) == "Wine")
+x <- wine[, -cls] # instances without classes
+y <- wine[, cls] # the classes
+x <- scale(x) # scale the attributes
+
+## Prepare data
+set.seed(20)
+# Use 50% of instances for training
+tra.idx <- sample(x = length(y), size = ceiling(length(y) * 0.5))
+xtrain <- x[tra.idx,] # training instances
+ytrain <- y[tra.idx]  # classes of training instances
+# Use 70% of train instances as unlabeled set
+tra.na.idx <- sample(x = length(tra.idx), size = ceiling(length(tra.idx) * 0.7))
+ytrain[tra.na.idx] <- NA # remove class information of unlabeled instances
+
+# Use the other 50% of instances for inductive testing
+tst.idx <- setdiff(1:length(y), tra.idx)
+xitest <- x[tst.idx,] # testing instances
+yitest <- y[tst.idx] # classes of testing instances
+
+## Example A: 
+# Training from a set of instances with 
+# 1-NN and C-svc (SVM) as base classifiers.
+
+### Define knn base classifier using knn3 from caret package
+library(caret)
+# learner function
+knn <- function(indexes, cls) {
+  knn3(x = xtrain[indexes, ], y = cls, k = 1)
+}
+# function to predict probabilities
+knn.prob <- function(model, indexes) {
+  predict(model, xtrain[indexes, ])
+}
+
+### Define svm base classifier using ksvm from kernlab package
+library(kernlab)
+# learner function
+svm <- function(indexes, cls) {
+  ksvm(x = xtrain[indexes, ], y = cls,
+       type = "C-svc",  C = 1,
+       kernel = "rbfdot", kpar = list(sigma = 0.048),
+       prob.model = TRUE)
+}
+# function to predict probabilities
+svm.prob <- function(model, indexes) {
+  predict(model, xtrain[indexes, ], type = "probabilities")
+}
+
+### Train
+set.seed(1)
+m1 <- democraticBase(y = ytrain, 
+                     learnersB = list(knn, svm), 
+                     predsB = list(knn.prob, svm.prob))
+### Predict
+# predict labels using each classifier
+m1.pred1 <- predict(m1$models[[1]], xitest, type = "class")
+m1.pred2 <- predict(m1$models[[2]], xitest)
+# combine predictions
+m1.pred <- list(m1.pred1, m1.pred2)
+cls1 <- democraticCombining(m1.pred, m1$W, m1$classes)
+caret::confusionMatrix(table(cls1, yitest))
+
+## Example B: 
+# Training from a distance matrix and a kernel matrix with 
+# 1-NN and C-svc (SVM) as base classifiers.
+
+### Define knn2 base classifier using oneNN from ssc package
+library(ssc)
+# Compute distance matrix D
+# D is used in knn2.prob and to compute kernel matrix
+library(proxy)
+D <- as.matrix(dist(x = xtrain, method = "euclidean", by_rows = TRUE))
+# learner function
+knn2 <- function(indexes, cls) {
+  model <- oneNN(y = cls)
+  attr(model, "tra.idxs") <- indexes
+  model
+}
+# function to predict probabilities
+knn2.prob <- function(model, indexes)  {
+  tra.idxs <- attr(model, "tra.idxs")
+  predict(model, D[indexes, tra.idxs],
+          type = "prob",  initial.value = 0) 
+}
+
+### Define svm2 base classifier using ksvm from kernlab package
+library(kernlab)
+
+# Compute kernel matrix K
+# K is used in svm2 and svm2.prob functions
+sigma <- 0.048
+K <- as.kernelMatrix(exp(- sigma * D * D))
+
+# learner function
+svm2 <- function(indexes, cls) {
+  model <- ksvm(K[indexes, indexes], y = cls, 
+                type = "C-svc", C = 1,
+                kernel = "matrix", 
+                prob.model = TRUE)
+  attr(model, "tra.idxs") <- indexes
+  model
+}
+# function to predict probabilities
+svm2.prob <- function(model, indexes)  {
+  tra.idxs <- attr(model, "tra.idxs")
+  sv.idxs <- tra.idxs[SVindex(model)]
+  predict(model, 
+          as.kernelMatrix(K[indexes, sv.idxs]),
+          type = "probabilities") 
+}
+
+### Train
+set.seed(1)
+m2 <- democraticBase(y = ytrain, 
+                     learnersB = list(knn2, svm2), 
+                     predsB = list(knn2.prob, svm2.prob))
+
+### Predict
+# Compute distance matrix Ditest
+Ditest <- dist(x = xitest, y = xtrain[m2$included.insts,],
+               method = "euclidean", by_rows = TRUE)
+class(Ditest) <- "matrix" # unable to change class using as.matrix
+# predict using classifier 1
+m2.pred1 <- predict(m2$models[[1]], Ditest[, m2$indexes[[1]]])
+
+# Compute kernel matrix Kitest
+Kitest <- exp(- sigma * Ditest * Ditest)
+# predict using classifier 2
+tra.idxs <- m2$indexes[[2]]
+sv.idxs <- tra.idxs[SVindex(m2$models[[2]])]
+m2.pred2 <- predict(m2$models[[2]], as.kernelMatrix(Kitest[, sv.idxs]))
+
+# Combine predictions
+m2.pred <- list(m2.pred1, m2.pred2)
+cls2 <- democraticCombining(m2.pred, m2$W, m2$classes)
+caret::confusionMatrix(table(cls2, yitest))
+
+
