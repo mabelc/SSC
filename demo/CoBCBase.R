3b97735b0c6c2a3d3c3124c444468bdaee34ad61
Osmani Rosado-Falcon <osmanir@uclv.cu>
2018-01-07 10:10:37 -0500

    Arreglado un error en los ejemplos de coBCBase.

--- demo/CoBCBase.R
+++ demo/CoBCBase.R
@@ -38,7 +38,7 @@ h.prob <- lapply(
   FUN = function(m) predict(m, xitest)
 )
 # Combine probability matrices
-prob <- coBCCombine(h.prob, ninstances, md1$classes)
+prob <- coBCCombine(h.prob, ninstances = nrow(xitest), md1$classes)
 # Get class per instance
 cls1 <- md1$classes[apply(prob, 1, which.max)]
 caret::confusionMatrix(table(cls1, yitest))
